1c33dbcc226bb7de24cd977540863ad7fb1556e4
numadog <maasamasamasamasanibosi@gmail.com>
2019-10-18 19:44:19 +0900

    no message

---
+++ Assets/script/time control/TimerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+
+    public Text timerText;
+    public float totalTime;
+    int seconds;
+    public bool ScreenCtrl = false;
+
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+        seconds = (int)totalTime;
+        timerText.text = seconds.ToString();
+
+        if (timerText.text == "0")
+        {
+            ScreenCtrl = true;
+        }
+    }
+}
---
+++ Assets/script/time control/screenControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class screenControler : MonoBehaviour
+{
+    public TimerController TimerController;
+    [SerializeField] GameObject TimePanel;
+    [SerializeField] GameObject TextPanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerController.ScreenCtrl == false)
+        {
+            TextPanel.SetActive(false);
+            TimePanel.SetActive(true);
+        }
+
+        else if (TimerController.ScreenCtrl == true)
+        {
+            TextPanel.SetActive(true);
+            TimePanel.SetActive(false);
+        }
+    }
+}
